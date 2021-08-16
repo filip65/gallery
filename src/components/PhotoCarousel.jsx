dd8d1c5202c491476b8cbd83b508cb78cedc800e
Filip Katusin <filip.katusin@gmail.com>
2021-08-16 18:36:54 +0200

    pridanie detailov pre prezeranie fotiek

--- src/components/PhotoCarousel.jsx
+++ src/components/PhotoCarousel.jsx
@@ -35,40 +35,42 @@ function PhotoCarousel({ images, index, setIndex }) {
   return (
     <div className="photoCarousel">
       <img src={imageUrl} alt="" />
-      <svg
-        className="prevIcon"
-        onClick={prevImage}
-        xmlns="http://www.w3.org/2000/svg"
-        width="15"
-        height="24"
-        viewBox="0 0 15 24"
-      >
-        <defs></defs>
-        <path
-          id="prev_icon"
-          data-name="prev icon"
-          class="cls-1"
-          d="M408.157,552l2.856-2.82L401.736,540l9.277-9.18L408.157,528,396,540Z"
-          transform="translate(-396 -528)"
-        />
-      </svg>{" "}
-      <svg
-        className="nextIcon"
-        onClick={nextImage}
-        xmlns="http://www.w3.org/2000/svg"
-        width="14.97"
-        height="24"
-        viewBox="0 0 14.97 24"
-      >
-        <defs></defs>
-        <path
-          id="next_icon"
-          data-name="next icon"
-          class="cls-1"
-          d="M1193.77,528l-2.85,2.82,9.26,9.18-9.26,9.18,2.85,2.82,12.13-12Z"
-          transform="translate(-1190.94 -528)"
-        />
-      </svg>
+      <button className="prevIcon">
+        <svg
+          onClick={prevImage}
+          xmlns="http://www.w3.org/2000/svg"
+          width="15"
+          height="24"
+          viewBox="0 0 15 24"
+        >
+          <defs></defs>
+          <path
+            id="prev_icon"
+            data-name="prev icon"
+            class="cls-1"
+            d="M408.157,552l2.856-2.82L401.736,540l9.277-9.18L408.157,528,396,540Z"
+            transform="translate(-396 -528)"
+          />
+        </svg>
+      </button>
+      <button className="nextIcon">
+        <svg
+          onClick={nextImage}
+          xmlns="http://www.w3.org/2000/svg"
+          width="14.97"
+          height="24"
+          viewBox="0 0 14.97 24"
+        >
+          <defs></defs>
+          <path
+            id="next_icon"
+            data-name="next icon"
+            class="cls-1"
+            d="M1193.77,528l-2.85,2.82,9.26,9.18-9.26,9.18,2.85,2.82,12.13-12Z"
+            transform="translate(-1190.94 -528)"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
