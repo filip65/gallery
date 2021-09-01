a8e10877471c5403427b5378185bbf136e6df0d5
filip65 <filip.katusin@gmail.com>
2021-09-01 14:06:42 +0200

    upravenie modali a pridanie esc funkcie

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,36 +1,30 @@
-import React, { useEffect } from "react";
+import React from "react";
 import closeIcon from "../images/close_icon.svg";
 import "../styles/Modal.scss";
 
-function Modal({ setIsOpen, children }) {
+import ReactModal from "react-modal";
+ReactModal.setAppElement("#root");
+
+function Modal({ setIsOpen, isOpen, children }) {
   const closeModal = () => {
     setIsOpen(false);
-    document.body.style.overflow = "visible";
   };
-
-  const closeModalEsc = (e) => {
-    if (e.code === "Escape") {
-      closeModal();
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", (e) => closeModalEsc(e));
-    return () => {
-      document.removeEventListener("keydown", (e) => closeModalEsc(e));
-    };
-  }, []);
 
   return (
-    <div className="modal">
-      <div className="container">
-        {children}
-        <button className="closeBtn" onClick={closeModal}>
-          <img src={closeIcon} alt="close icon" />
-          Zavrieť
-        </button>
-      </div>
-    </div>
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={() => setIsOpen(false)}
+      closeModalEsc={true}
+      shouldCloseOnOverlayClick={false}
+      overlayClassName={"modal"}
+      className={"container"}
+    >
+      {children}
+      <button className="closeBtn" onClick={closeModal}>
+        <img src={closeIcon} alt="close icon" />
+        Zavrieť
+      </button>
+    </ReactModal>
   );
 }
 
