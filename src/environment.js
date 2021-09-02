6b84410d182486924e779d93304aa6591a6b48d0
filip65 <filip.katusin@gmail.com>
2021-09-02 19:06:25 +0200

    odstranenie komentu

--- src/environment.js
+++ src/environment.js
@@ -1,11 +1,3 @@
 export const environment = {
   apiUrl: "http://api.programator.sk",
-
-  // getImages = async (path) => {
-  //     const data = fetch(`${apiUrl}/gallery`)
-  //     .then(res => res.json())
-  //     .then(data => data.galleries)
-
-  //     return data;
-  // }
 };
