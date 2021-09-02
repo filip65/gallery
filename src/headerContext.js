186366e65d8c0e911c0515f44f4fc9808dedab5c
filip65 <filip.katusin@gmail.com>
2021-09-02 13:19:03 +0200

    pridanie contextApi pre header

---
+++ src/headerContext.js
@@ -0,0 +1,27 @@
+import React, { useState, useContext } from "react";
+
+const HeaderContext = React.createContext();
+
+const HeaderProvider = ({ children }) => {
+  const [headerBg, setHeaderBg] = useState("");
+
+  const changeHeaderBg = (value) => {
+    if (value[0] === "#") {
+      setHeaderBg(value);
+    } else {
+      setHeaderBg(`url(${value})`);
+    }
+  };
+
+  return (
+    <HeaderContext.Provider value={{ headerBg, changeHeaderBg }}>
+      {children}
+    </HeaderContext.Provider>
+  );
+};
+
+const useGetHeaderBg = () => {
+  return useContext(HeaderContext);
+};
+
+export { HeaderProvider, useGetHeaderBg };
